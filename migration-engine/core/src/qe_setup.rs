1b05191b091f8e79e29512045560a7bccda2b1b3
Tom Houlé <tom@tomhoule.com>
2021-05-19 16:44:27 +0200

    [release] Remove special exit code handling in ME CLI

--- migration-engine/core/src/qe_setup.rs
+++ migration-engine/core/src/qe_setup.rs
@@ -14,11 +14,12 @@ use mongodb_migration_connector::MongoDbMigrationConnector;
 use sql_migration_connector::SqlMigrationConnector;
 
 /// Flags from Query Engine to define the underlying database features.
-#[derive(Debug, Clone, Copy, BitFlags)]
+#[enumflags2::bitflags]
+#[derive(Debug, Clone, Copy)]
 #[repr(u8)]
 pub enum QueryEngineFlags {
     /// We cannot `CREATE` (or `DROP`) databases.
-    DatabaseCreationNotAllowed = 0x01,
+    DatabaseCreationNotAllowed,
 }
 
 /// Database setup for connector-test-kit.
